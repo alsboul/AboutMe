311e01c266631a61910c93cf4357e40b754e3e90
mohammad alsboul <mohammad.alspoole@gmail.com>
2019-11-16 12:02:56 +0200

    done

--- js/app.js
+++ js/app.js
@@ -1,130 +1,133 @@
-'use strict'
 function getName(){
-var name = prompt('Hello can you tell me your name please');
-alert(name + ' Welcome To my page ');
+  var name = prompt('Hello can you tell me your name please');
+  alert (name + ' Welcome To my page ');
 }
 getName();
 alert('Please answer my qustion with yes or no!');
-var point=0;
+var point = 0;
 function getQus1(){
-var qus1 = prompt('do you guess my age is 23?');
-if (qus1 === 'yes' || qus1 === 'y' ) {
+  var qus1 = prompt('do you guess my age is 23?');
+  if (qus1 === 'yes' || qus1 === 'y' ) {
     qus1 = qus1.toUpperCase();
     alert('Oh you are my man');
-   
+
     point++;
-}
-else {
+  }
+  else {
     alert('You need to know more about me');
-}
+  }
 }
 getQus1();
 //console.log('your answer is =', qus1);
 function getQus2(){
-var qus2 = prompt('Do you think my favourite color is green? ');
-if (qus2 === 'no' || qus2 === 'n') {
+  var qus2 = prompt('Do you think my favourite color is green? ');
+  if (qus2 === 'no' || qus2 === 'n') {
     qus2 = qus2.toUpperCase();
     alert('Nice guessing');
     point++;
-}
-else {
+  }
+  else {
     alert(' Sorry this is not my favourite color');
-    
-}
+
+  }
 }
 getQus2();
 //console.log('your answer is =', qus2);
 function getQus3(){
-var qus3 = prompt(' Are my actions guided by love');
-if (qus3 === 'yes' || qus3 === 'y' ) {
+  var qus3 = prompt(' Are my actions guided by love');
+  if (qus3 === 'yes' || qus3 === 'y' ) {
     qus3 = qus3.toUpperCase();
     alert('Thats right');
     point++;
-}
-else {
+  }
+  else {
     alert('You need to be careful about your answer');
-}
+  }
 }
 getQus3();
 //console.log('your answer is =', qus3);
 function getQus4(){
-var qus4 = prompt(' Do you think i love mansef?');
-if (qus4 === 'yes' || qus4 === 'y' ) {
+  var qus4 = prompt(' Do you think i love mansef?');
+  if (qus4 === 'yes' || qus4 === 'y' ) {
     qus4 = qus4.toUpperCase();
     alert('You are my frined');
     point++;
-}
-else {
+  }
+  else {
     alert('try again my frinde');
-}
+  }
 }
 getQus4();
 //console.log('your answer is =', qus4);
 function getQus5(){
-var qus5 = prompt(' Do you think i love pc games?');
-if (qus5 === 'yes' || qus5 === 'y' ) {
+  var qus5 = prompt(' Do you think i love pc games?');
+  if (qus5 === 'yes' || qus5 === 'y' ) {
     qus5 = qus5.toUpperCase();
     alert('You are my lovely man');
-    point++
-}
-else {
+    point++;
+  }
+  else {
     alert('No man who dose not love pc games');
-}
+  }
 }
 getQus5();
 //console.log('your answer is =', qus5);
 function getQus6(){
-alert('Are you redy to guss number from 1 to 10?');
-for(var num=0; num<=3; num++){
+  alert('Are you redy to guss number from 1 to 10?');
+  for(var num = 0; num <= 3; num++){
     var qus6 = prompt('guess what the number in my mind?');
-    if(qus6 == 3){
-        alert('thats right');
-        num=3;
+    if(qus6 === 3){
+      alert('thats right');
+      num = 3;
     }
     else if (qus6 < 3){
-        alert('too low');
+      alert('too low');
     }
     else if (qus6 > 3){
-        alert('too high');
-    }   
+      alert('too high');
+    }
 
-}
+  }
 
-   alert('the answer is 3');
-   point++
+  alert('the answer is 3');
+  point++;
 }
 getQus6();
 function getQus7(){
-   var contry = ["rome","paris","london","barcelona"];
-   var flag=0;
-   for(var i=0; i<4 ; i++)
-{
+  var contry = ['rome','paris','london','barcelona'];
+  var flag = 0;
+  for(var i = 0; i < 4 ; i++)
+  {
     var qus7 = prompt('guess what is my favourite contry to vist');
-    
-    
-    for(var j =0; j<4; j++)
-    {if(qus7 == contry[j]){
-        
+
+
+    for(var j = 0; j < 4; j++)
+    {
+      if(qus7 === contry[j])
+      {
+
         alert('right,you are my man');
-        j=5;
-        flag=10;
-        point++
-    }
+        j = 5;
+        flag = 10;
+        point++;
+      }
 
     }
-    if (flag == 10)
+    if (flag === 10)
     {
-        i=10;
+      i = 10;
     }
     else {
-   alert('try again');}
-   
-
+      alert('try again');
     }
- }
- getQus7();
- alert('your score is ' +point+ ' out of 7')
-   
+
+
+  }
+}
+getQus7();
+alert('your score is ' + point + ' out of 7');
+
+// eslint-disable-next-line no-undef
 alert(contry);
 alert('Thank you for answer my qustion and welcome to my page again ' + name);
 
